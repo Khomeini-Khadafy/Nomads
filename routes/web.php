695a56c8174ea0ff82f251d177f7c946ca2f5c63
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-02-03 20:38:08 +0700

    layouts halaman admin, dan menambahkan namespace di AppServiceProvider

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function() {
+        Route::get('/', [DashboardController::class, 'index'])->name('Admin');
+    });
