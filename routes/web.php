a7e90216537ae902ece2120f12db7b00527196f2
Khomeini Khadafy <Khomeini,khadafy@gmail.com>
2025-02-04 16:56:38 +0700

    menambahkan Page.home kedalam route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
 
 Route::prefix('admin')->group(function() {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
